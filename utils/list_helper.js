04a80e889a98d5f160776e879e02fc2966e2013d
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-06-02 11:28:53 -0300

    Excersice 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reduce = (suma, item) => suma + item.likes;
+
+  return blogs.reduce(reduce, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
