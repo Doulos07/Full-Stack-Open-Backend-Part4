4f1adf191f619e5d3b5ad9fc4e709b664341c982
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-06-02 11:07:19 -0300

    more endpoints and functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 const app = express();
 
 // Conection mongoose
@@ -19,7 +20,11 @@ mongoose
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint);
 
 module.exports = app;
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,18 +2,57 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blogs");
 
 blogsRouter.get("/", (request, response, next) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((returnBlog) => {
+      if (returnBlog) {
+        response.json(returnBlog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 blogsRouter.post("/", (request, response, next) => {
   console.log(request.body);
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
+
+blogsRouter.put("/:id", (request, response, next) => {
+  Blog.findByIdAndUpdate(request.params.id, request.body, {
+    returnDocument: "after",
+  })
+    .then((updateBlog) => {
+      if (updateBlog) {
+        response.json(updateBlog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.delete("/:id", (request, response, next) =>
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error)),
+);
 
 module.exports = blogsRouter;
