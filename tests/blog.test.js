795217a4ce259f10379ee219b1de0ff83f89b273
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-06-05 14:58:37 -0300

    Add describe test

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -1,5 +1,5 @@
 const assert = require("node:assert");
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
@@ -8,82 +8,91 @@ const helper = require("./test_helper");
 
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  await Promise.all(helper.initialBlogs.map((blog) => new Blog(blog).save()));
-});
+describe("blog api", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Promise.all(helper.initialBlogs.map((blog) => new Blog(blog).save()));
+  });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+  describe("validating format", () => {
+    test("blogs are returned as json", async () => {
+      await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+    });
 
-test("Validate the Blogs lathe without _id", async () => {
-  const response = await api.get("/api/blogs");
+    test("Validate the Blogs lathe without _id", async () => {
+      const response = await api.get("/api/blogs");
 
-  // const has_id = response.body.some((blog) => blog.hasOwnProperty("_id")); -> ESLint -> Error | robustness problem
-  const has_id = response.body.some((blog) =>
-    Object.prototype.hasOwnProperty.call(blog, "_id"),
-  );
-  assert.strictEqual(has_id, false);
-});
+      // const has_id = response.body.some((blog) => blog.hasOwnProperty("_id")); -> ESLint -> Error | robustness problem
+      const has_id = response.body.some((blog) =>
+        Object.prototype.hasOwnProperty.call(blog, "_id"),
+      );
+      assert.strictEqual(has_id, false);
+    });
+  });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "Jijiji",
-    author: "Patricio Rey y sus Redonditos de Ricota",
-    url: "https://open.spotify.com/track/1tW6LiJGXGlReuNP38wrKb?si=6f9bbcd2ca4c439a",
-    likes: 20,
-  };
+  describe("addition of a new blog", () => {
+    test("a valid blog can be added", async () => {
+      const newBlog = {
+        title: "Jijiji",
+        author: "Patricio Rey y sus Redonditos de Ricota",
+        url: "https://open.spotify.com/track/1tW6LiJGXGlReuNP38wrKb?si=6f9bbcd2ca4c439a",
+        likes: 20,
+      };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-  const blogs = await helper.blogsInDb();
+      const blogs = await helper.blogsInDb();
 
-  assert.strictEqual(blogs.length, helper.initialBlogs.length + 1);
-});
+      assert.strictEqual(blogs.length, helper.initialBlogs.length + 1);
+    });
 
-test("validate key likes", async () => {
-  const newBlog = {
-    title: "La Ultima Lagrima",
-    author: "Memphis la Blusera",
-    url: "https://open.spotify.com/track/0cHVi2rirbT62DlX3uabke?si=6a084c99fceb40b4",
-  };
+    test("validate key likes", async () => {
+      const newBlog = {
+        title: "La Ultima Lagrima",
+        author: "Memphis la Blusera",
+        url: "https://open.spotify.com/track/0cHVi2rirbT62DlX3uabke?si=6a084c99fceb40b4",
+      };
 
-  const response = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      const response = await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-  assert.strictEqual(response.body.likes, 0);
-});
+      assert.strictEqual(response.body.likes, 0);
+    });
 
-test("validate title and url", async () => {
-  const newBlogUrl = {
-    title: "La Ultima Lagrima",
-    author: "Memphis la Blusera",
-  };
+    test("validate title and url", async () => {
+      const newBlogUrl = {
+        title: "La Ultima Lagrima",
+        author: "Memphis la Blusera",
+      };
 
-  const newBlogTitle = {
-    author: "Memphis la Blusera",
-    url: "https://open.spotify.com/track/0cHVi2rirbT62DlX3uabke?si=6a084c99fceb40b4",
-  };
+      const newBlogTitle = {
+        author: "Memphis la Blusera",
+        url: "https://open.spotify.com/track/0cHVi2rirbT62DlX3uabke?si=6a084c99fceb40b4",
+      };
 
-  const responseUrl = await api.post("/api/blogs").send(newBlogUrl).expect(400);
-  const responseTitle = await api
-    .post("/api/blogs")
-    .send(newBlogTitle)
-    .expect(400);
+      const responseUrl = await api
+        .post("/api/blogs")
+        .send(newBlogUrl)
+        .expect(400);
+      const responseTitle = await api
+        .post("/api/blogs")
+        .send(newBlogTitle)
+        .expect(400);
 
-  assert(responseUrl.body.error.includes("url is required"));
-  assert(responseTitle.body.error.includes("title is required"));
+      assert(responseUrl.body.error.includes("url is required"));
+      assert(responseTitle.body.error.includes("title is required"));
+    });
+  });
 });
 
 after(async () => {
