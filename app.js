44c5cfad8e4f88cf7b30e609cb0ff2da56ce48ce
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-06-08 14:24:11 -0300

    Excersice 4.18

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 require("express-async-errors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const app = express();
@@ -26,6 +27,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
